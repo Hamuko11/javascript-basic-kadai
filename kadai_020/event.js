9659b4634e20e03b5e07ebd0c60688fa7896ea3f
Hamuko11 <167842193+Hamuko11@users.noreply.github.com>
2024-05-13 11:42:41 +0900

    kadai_020をアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+//ボタン要素を定数に代入する
+const Btn = document.getElementById('btn');
+//クリックでイベント処理
+Btn.addEventListener('click', () => {
+  //h2の文字列を取得する
+  const h2Text =document.getElementById('text').innerHTML="<h2>ボタンをクリックしました</h2>"
+});
